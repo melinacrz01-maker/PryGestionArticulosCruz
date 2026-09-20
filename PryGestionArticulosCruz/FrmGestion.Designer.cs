6b639acc36b6a994f1b5a0ece8bf5a4ab3948e70
Lore <Lore@DESKTOP-87UNB4R>
2026-09-19 21:39:33 -0300

    Add project files.

---
+++ PryGestionArticulosCruz/FrmGestion.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace PryGestionArticulosCruz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.grpDatos = new System.Windows.Forms.GroupBox();
+            this.lblRubro = new System.Windows.Forms.Label();
+            this.cmbArticulos = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.lblcantidadarticulos = new System.Windows.Forms.Label();
+            this.lblcantArticulo = new System.Windows.Forms.Label();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grpDatos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpDatos
+            // 
+            this.grpDatos.BackColor = System.Drawing.Color.Lavender;
+            this.grpDatos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.grpDatos.Controls.Add(this.label1);
+            this.grpDatos.Controls.Add(this.lblTotal);
+            this.grpDatos.Controls.Add(this.lblcantArticulo);
+            this.grpDatos.Controls.Add(this.lblcantidadarticulos);
+            this.grpDatos.Controls.Add(this.lblCantidad);
+            this.grpDatos.Controls.Add(this.dataGridView1);
+            this.grpDatos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpDatos.ForeColor = System.Drawing.Color.Navy;
+            this.grpDatos.Location = new System.Drawing.Point(25, 106);
+            this.grpDatos.Name = "grpDatos";
+            this.grpDatos.Size = new System.Drawing.Size(582, 330);
+            this.grpDatos.TabIndex = 2;
+            this.grpDatos.TabStop = false;
+            this.grpDatos.Text = "Articulos";
+            this.grpDatos.Enter += new System.EventHandler(this.grpDatos_Enter);
+            // 
+            // lblRubro
+            // 
+            this.lblRubro.AutoSize = true;
+            this.lblRubro.BackColor = System.Drawing.Color.Transparent;
+            this.lblRubro.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRubro.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lblRubro.Location = new System.Drawing.Point(22, 61);
+            this.lblRubro.Name = "lblRubro";
+            this.lblRubro.Size = new System.Drawing.Size(49, 16);
+            this.lblRubro.TabIndex = 1;
+            this.lblRubro.Text = "Rubro";
+            // 
+            // cmbArticulos
+            // 
+            this.cmbArticulos.FormattingEnabled = true;
+            this.cmbArticulos.Location = new System.Drawing.Point(77, 61);
+            this.cmbArticulos.Name = "cmbArticulos";
+            this.cmbArticulos.Size = new System.Drawing.Size(163, 21);
+            this.cmbArticulos.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.Location = new System.Drawing.Point(21, 39);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(538, 218);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidad.Location = new System.Drawing.Point(18, 283);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(155, 16);
+            this.lblCantidad.TabIndex = 3;
+            this.lblCantidad.Text = "Cantidad de Articulos";
+            // 
+            // lblcantidadarticulos
+            // 
+            this.lblcantidadarticulos.AutoSize = true;
+            this.lblcantidadarticulos.Location = new System.Drawing.Point(179, 319);
+            this.lblcantidadarticulos.Name = "lblcantidadarticulos";
+            this.lblcantidadarticulos.Size = new System.Drawing.Size(0, 15);
+            this.lblcantidadarticulos.TabIndex = 4;
+            // 
+            // lblcantArticulo
+            // 
+            this.lblcantArticulo.AllowDrop = true;
+            this.lblcantArticulo.BackColor = System.Drawing.Color.GhostWhite;
+            this.lblcantArticulo.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.lblcantArticulo.Location = new System.Drawing.Point(179, 277);
+            this.lblcantArticulo.Name = "lblcantArticulo";
+            this.lblcantArticulo.Size = new System.Drawing.Size(136, 27);
+            this.lblcantArticulo.TabIndex = 5;
+            this.lblcantArticulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblcantArticulo.UseWaitCursor = true;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.BackColor = System.Drawing.Color.Navy;
+            this.btnMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrar.ForeColor = System.Drawing.Color.AliceBlue;
+            this.btnMostrar.Location = new System.Drawing.Point(246, 49);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(147, 41);
+            this.btnMostrar.TabIndex = 6;
+            this.btnMostrar.Text = "Mostrar Articulos";
+            this.btnMostrar.UseVisualStyleBackColor = false;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Código";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Descripción";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Costo";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Stock";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Valor en Stock";
+            this.Column5.Name = "Column5";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(347, 283);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(43, 16);
+            this.lblTotal.TabIndex = 6;
+            this.lblTotal.Text = "Total";
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.BackColor = System.Drawing.Color.GhostWhite;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Location = new System.Drawing.Point(396, 277);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 27);
+            this.label1.TabIndex = 7;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.UseWaitCursor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(619, 25);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.BackColor = System.Drawing.Color.Navy;
+            this.salirToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salirToolStripMenuItem.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(47, 21);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(619, 448);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.grpDatos);
+            this.Controls.Add(this.lblRubro);
+            this.Controls.Add(this.cmbArticulos);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Bienvenido";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.grpDatos.ResumeLayout(false);
+            this.grpDatos.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbArticulos;
+        private System.Windows.Forms.Label lblRubro;
+        private System.Windows.Forms.GroupBox grpDatos;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Label lblcantidadarticulos;
+        private System.Windows.Forms.Label lblcantArticulo;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+    }
+}
+
---
+++ PryGestionArticulosCruz/FrmGestion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PryGestionArticulosCruz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void grpDatos_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult respuesta;
+
+            respuesta = MessageBox.Show(
+                "¿Desea salir del programa?",
+                "Confirmar salida",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (respuesta == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+    
+    
+}
